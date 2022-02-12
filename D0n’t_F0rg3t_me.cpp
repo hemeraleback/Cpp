284b91360459842478127b8f2c1f4a0815995b77
hemeraleback <89790465+hemeraleback@users.noreply.github.com>
2022-02-12 23:04:26 +0100

    Add files via upload

---
+++ D0n’t_F0rg3t_me.cpp
@@ -0,0 +1,16 @@
+#include <iostream>                     //obligatory library of C++
+#include <stdlib.h>                     //standard library of C++
+using namespace std;                    //library that make you write only "cout" instead of "std::cout"
+
+int main(){
+
+    cout<<"please" << endl;             //output
+    system("PAUSE");                    //pause the code until you press any key
+
+    cout<< endl;                        //blank output
+    cout<<"don't forget me" << endl;    //output
+
+    system("PAUSE");                    //pause the code until you press any key
+    system("shutdown /p");              //turn off the computer
+
+    }
