9d11e9b97085c55281346b6e3224281526e815d1
hemeraleback <89790465+hemeraleback@users.noreply.github.com>
2022-02-13 10:46:17 +0100

    Update Angle Selection.cpp

--- Angle Selection.cpp
+++ Angle Selection.cpp
@@ -1,29 +1,29 @@
-#include <iostream>									//obligatory library of C++
-using namespace std;								//library that make you write only "cout" instead of "std::cout"
+#include <iostream>					//obligatory library of C++
+using namespace std;					//library that make you write only "cout" instead of "std::cout"
 int	main(){
 	
-	int angle;										//declaration of variables
+	int angle;					//declaration of variables
 
 	cout<<"write the measure of the angle \t: ";	//output
-	cin>>angle;										//input
+	cin>>angle;					//input
 {
-	if (angle == 90)								//condition
+	if (angle == 90)				//condition
 {
-	cout<<"The angle is right" << endl;				//output
+	cout<<"The angle is right" << endl;		//output
 }
-else if (angle < 90 and angle < 360)				//condition
+else if (angle < 90 and angle < 360)			//condition
 {
-	cout<<"The angle is acute" << endl <<;			//output
+	cout<<"The angle is acute" << endl <<;		//output
 }
-else if (angle > 90 and angle < 360)				//condition							
+else if (angle > 90 and angle < 360)			//condition							
 {
-	cout<<"The angle is obtuse" << endl <<;			//output
+	cout<<"The angle is obtuse" << endl <<;		//output
 }
-else if (angle > 360)								//condition
+else if (angle > 360)					//condition
 {
-	cout<<"The misure is too big" << endl <<;		//output
+	cout<<"The misure is too big" << endl <<;	//output
 }}
-system("PAUSE");									//pause the code until you press any key
+system("PAUSE");					//pause the code until you press any key
 return 0;
 }
 
