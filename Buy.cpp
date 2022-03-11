0016c62dcfa89d1b2c3afa9104f71ba602811170
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-03-11 18:37:31 +0100

    Update Buy.cpp

--- Buy.cpp
+++ Buy.cpp
@@ -1,32 +1,32 @@
-#include <iostream>                                                                 	//obligatory library of C++
-using namespace std;                                                                	//library that make you write only "cout" instead of "std::cout"
+#include <iostream>
+using namespace std;
 
 int main() {
 	
-	int price, saving, add, rest;                                                   //declaration of variables
+	int price, saving=0, add, rest;
 	
-	saving = 0;                                                                     //assing the value as 0
+	cout <<"Enter the price \t : " << endl;
+	cin >> price;
 	
-	cout <<"Enter the price \t : " << endl;                                         //output
-	cin >> price ;                                                                  //input
-	
-	do{                                                                             //start of a do-while
+	do{
 
-		cout<<"How many money do you want to add? \t : " << endl;               //output              
-		cin >> add ;                                                            //input
+		cout<<"How many money do you want to add? \t : " << endl;           
+		cin >> add;
 
-		saving += add;                                                          //calcultions
+		saving += add;
 
 	}
-	while(saving < price);                                                          //end of a do-while
+	while(saving < price);
 	
-	rest = saving - price;                                                          //calculations
+	rest = saving - price;
 	
-	if(rest == 0){                                                                  //conditions
-		cout<<"Now you can buy it" << endl;                                     //output
-    }else{                                                                          	//if condition isn' true
-		cout<<"Now you can buy it and you saved " << rest << " euro" << endl;   //output
-    }
-	system("PAUSE");                                                                //pause the code until you press any key
+	if(rest == 0){
+		cout<<"Now you can buy it" << endl;
+    	}
+	else
+	{
+		cout<<"Now you can buy it and you saved " << rest << " euro" << endl;
+   	}
+	system("PAUSE");
 	return 0;
 }
