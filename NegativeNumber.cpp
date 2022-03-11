743292e6a9bc4edc752c4f3b8284f1028c31a383
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-03-11 18:49:18 +0100

    Update NegativeNumber.cpp

--- NegativeNumber.cpp
+++ NegativeNumber.cpp
@@ -1,23 +1,25 @@
-#include <iostream>                                                                                     //obligatory library of C++
-using namespace std;                                                                                    //library that make you write only "cout" instead of "std::cout"
+#include <iostream>
+using namespace std;
 
 int main(){
 
-    int num1;                                                                                           //declaration of variables
+    int num;
 
-    cout << "Enter the number you want to know if is positive or negative \t:";                         //output
-    cin >> num1;                                                                                        //input
+    cout << "Enter the number you want to know if is positive or negative \t:";
+    cin >> num;
 
-    if (num1 > 0 )                                                                                      //condtion
-    {
-        cout << "Your Number is negative" << endl;                                                      //output
+    if (num > 0 ){
+        cout << "Your Number is negative" << endl;
     }
-    else if (num1 < 0 )                                                                                 //condition
-    {
-	    cout << "Your Number is positive " << endl;                                                 //output
+	
+    if (num < 0 ){
+	    cout << "Your Number is positive " << endl;
     }
-	else                                                                                            //if condition isn't true
-        cout << "Your Number is not positive and not negative because is equal to zero " << endl;       //output
-    system("PAUSE");                                                                                    //pause the code until you press any key
+    
+    if (num == 0){
+        cout << "Your Number is not positive and not negative because is equal to zero " << endl;
+    }
+	
+    system("PAUSE");
     return 0;
 }
