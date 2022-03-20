e73c96c48e0ccf68791f97c717932be594e28e1c
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-03-20 16:01:50 +0100

    Update NegativeNumber.cpp

--- NegativeNumber.cpp
+++ NegativeNumber.cpp
@@ -8,11 +8,11 @@ int main(){
     cout << "Enter the number you want to know if is positive or negative \t:";
     cin >> num;
 
-    if (num > 0 ){
+    if (num < 0 ){
         cout << "Your Number is negative" << endl;
     }
 	
-    if (num < 0 ){
+    if (num > 0 ){
 	    cout << "Your Number is positive " << endl;
     }
     
