e01957b0c25d3d37dfddd440d068f2dee20bdbc5
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-05-01 20:50:25 +0200

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -2,30 +2,47 @@
 using namespace std;
 
 int main(){
-    double n1, n2;
+    double n1, n2, sum, i=1;
     char ol;
     bool exit;
 
     do {
-        cout<<"Enter what type of operation you want to do [+,-,*,/]: ";
+        cout<<"Enter what type of operation you want to do [+,-,*,/,^]: ";
         cin>>ol;
 
-        if (ol != '+' && ol != '-' && ol != '/' && ol != '*')
+        //If the operatior isn't valid give an error message and let the user write it again
+        if (ol != '+' && ol != '-' && ol != '/' && ol != '*' && ol != '^')
         {
             cout<<"Your operator is invalid, try again"<<endl;
         }
         else
         {
+            //exit the loop if operator is valid
             exit = true;
         }
     }
     while (exit == false);
 
-    cout<<"Enter the first number: ";
-    cin>>n1;
+    if (ol != '^')
+    {
+        cout<<"Enter the first number: ";
+        cin>>n1;
 
-    cout<<"Enter the second number: ";
-    cin>>n2;
+        cout<<"Enter the second number: ";
+        cin>>n2;
+    }
+    else
+    {
+        cout<<"Enter the number: ";
+        cin>>n1;
+
+        cout<<"Enter the exponent: ";
+        cin>>n2;
+
+        for(sum = n1;i!=n2;i++){
+        sum = sum * n1;
+        }
+    }
 
     switch (ol)
     {
@@ -33,7 +50,11 @@ int main(){
         case '-':cout<<n1 << " - " << n2 << " = " << (n1-n2); break;
         case '*':cout<<n1 << " * " << n2 << " = " << (n1*n2); break;
         case '/':cout<<n1 << " / " << n2 << " = " << (n1/n2); break;
-    }
+        case '^':cout<<n1 << "^" << n2 << " = " << sum ; break;
+    };
     
+    cout<<endl;
+
+    system("PAUSE");
     return 0;
 }
