89040aa5e19b32ef1b111a7011adfcbdf9919ba0
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-11-27 18:41:29 +0100

    Update Average Calculator.cpp

--- Average Calculator.cpp
+++ Average Calculator.cpp
@@ -2,22 +2,20 @@
 using namespace std;
 int main() {
 	
-	double numbers, number=0, average, i=0;
+	double sum, number=0, average, i=0;
 	char stop='y';
 	
 	for(;stop == 'y'; i++){
 		cout<<"Enter the number:";
 		cin>>number;
 		
-		numbers = number + numbers;
+		sum += number;
 		
 		cout<<"Do you want to enter any other number? :";
 		cin>>stop;
 	};
 	
-	average = numbers/i;
-	
-	cout<<"Your average is " << average << endl;
+	cout<<"Your average is " << sum/i << endl;
 	
 	system("PAUSE");
 	return 0;
