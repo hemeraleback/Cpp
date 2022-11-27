64ba653d2a2c80cfc6d7f4d41f1d59fc87a89427
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-11-27 18:39:32 +0100

    Update Angle Selection.cpp

--- Angle Selection.cpp
+++ Angle Selection.cpp
@@ -2,27 +2,25 @@
 using namespace std;
 int main(){
 	
-	int angle;
-
-	cout<<"Write the measure of the angle \t: ";
-	cin>>angle;
-
-	if (angle == 90){
+	int angle = 361;
+    
+    while( angle > 360){
+	    cout<<"Write the measure of the angle : ";
+	    cin>>angle;
+	    
+	    if (angle > 360)
+	    cout<<"The misure is too big" << endl;
+    }
+    
+	if (angle == 90)
 	cout<<"The angle is right" << endl;
-	}
 	
-	if (angle < 90 && angle < 360){
-	cout<<"The angle is acute" << endl <<;
-	}
+	if (angle < 90)
+	cout<<"The angle is acute" << endl;
 	
-	if (angle > 90 && angle < 360){
-	cout<<"The angle is obtuse" << endl <<;	
-	}
+	if (angle > 90)
+	cout<<"The angle is obtuse" << endl;	
 	
-	if (angle > 360){
-	cout<<"The misure is too big" << endl <<;
-	}
-
 	system("PAUSE");
 	return 0;
 }
