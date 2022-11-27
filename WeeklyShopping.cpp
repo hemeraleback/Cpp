8a35c94af1450cbde2d4c29a3b575745821625b2
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-11-27 19:06:27 +0100

    Update WeeklyShopping.cpp

--- WeeklyShopping.cpp
+++ WeeklyShopping.cpp
@@ -4,22 +4,22 @@ int	main(){
 
     int distance, consume, liter, travel, daily, weekly;
 
-    cout<<"Enter the distance you need to travel" << endl;
+    cout<<"Enter the distance you need to travel"<<endl<<"-->";
     cin>>distance;
 
-    cout<<"Enter how much you consume for each kilometer" << endl;
+    cout<<"Enter how much you consume for each kilometer"<<endl<<"-->";
     cin>>consume;
 
-    cout<<"Enter how much you spend on a liter" << endl;
+    cout<<"Enter how much you spend on a liter" <<endl<<"-->";
     cin>>liter;
 
     travel=consume*distance;
     daily=travel*liter;
     weekly=daily*6;
 
-    cout<<"The consumption for each trip is \t: " << travel << " euros" << endl;
-    cout<<"The daily cost is \t: " << daily << " euros" << endl;
-    cout<<"The weekly cost is \t: " << weekly << "euros " << endl;
+    cout<<"The consumption for each trip is " << travel << " euros" << endl;
+    cout<<"The daily cost is " << daily << " euros" << endl;
+    cout<<"The weekly cost is " << weekly << "euros " << endl;
 
 system("PAUSE");
 return 0;
