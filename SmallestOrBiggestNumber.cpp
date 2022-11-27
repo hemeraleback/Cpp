f8b362af2d47c89adc41f635840201ff3273fd2e
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-11-27 19:04:46 +0100

    Update SmallestOrBiggestNumber.cpp

--- SmallestOrBiggestNumber.cpp
+++ SmallestOrBiggestNumber.cpp
@@ -4,19 +4,16 @@ int main()
 {
     double num1, num2;
 
-    cout << "Enter the first number \t: ";
+    cout << "Enter the first number: ";
     cin >> num1;
     
-    cout << "Enter the second number \t: ";
+    cout << "Enter the second number: ";
     cin >> num2;
 
-    if (num1 > num2){
+    if (num1 < num2)
         cout << "The smallest number is " << num1 << " and the biggest number is " << num2 << endl;
-    }
-    
-    if (num1 < num2){
+    else
         cout << "The smallest number is " << num2 << " and the biggest number is " << num1 << endl;
-    }
 
     system("PAUSE");
     return 0;
