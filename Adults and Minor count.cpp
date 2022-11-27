75b46a2908de8d08d99df87360b678a09055d024
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-11-27 18:36:56 +0100

    Update Adults and Minor count.cpp

--- Adults and Minor count.cpp
+++ Adults and Minor count.cpp
@@ -3,23 +3,21 @@ using namespace std;
 int main(){
 
     int Adults=0, Minor=0, Age;
-    char Name, Stop;
-
+    char Stop='n';
+    string Name;
+    
     do{
         
-        cout<<"Enter the name \t:";
+        cout<<"Enter the name:";
         cin>>Name;
 
-        cout<<"Enter the age \t: ";
+        cout<<"Enter the age: ";
         cin>>Age;
 
-        if (Age >= 18){
+        if (Age >= 18)
             Adults++;
-        }
         else
-        {
             Minor++;
-        }
         
         cout<<"Do You want to add other people [y/n] \t:";
         cin>>Stop;
