001e5047c5d9f74c88a9ee8401684d8cd8898d18
Hemerale Back <89790465+hemeraleback@users.noreply.github.com>
2022-12-01 18:20:53 +0100

    Update Adults and Minor count.cpp

--- Adults and Minor count.cpp
+++ Adults and Minor count.cpp
@@ -4,12 +4,8 @@ int main(){
 
     int Adults=0, Minor=0, Age;
     char Stop='n';
-    string Name;
     
     do{
-        
-        cout<<"Enter the name:";
-        cin>>Name;
 
         cout<<"Enter the age: ";
         cin>>Age;
